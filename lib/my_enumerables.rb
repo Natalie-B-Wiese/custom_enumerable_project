cbf6683c0fb195caa4b327c876c7944b35f6927c
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-04-02 13:33:21 -0400

    Implement my_each and my_each_with index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,18 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    # returns an 'Enumerator' bound to this method if a block isn't given
+    return to_enum(:my_each_with_index) unless block_given?
+
+    index=0
+
+    self.my_each do |element|
+      yield [element, index]
+      index+=1
+    end
+    
+  end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +21,13 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    # returns an `Enumerator` bound to this method if a block isn't given
+    return to_enum(:my_each) unless block_given?
+
+    self.each do |elem|
+      yield elem
+    end
+
+  end
 end
